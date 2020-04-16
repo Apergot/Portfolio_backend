e43f541b7fca98ea186523dd630598c1ba6dad6a
Alejandro Perdomo González <apergot95@gmail.com>
2020-04-16 15:06:35 +0100

    some important fixes i guess

--- src/main/java/com/apergot/springbootrestapi/SpringBootRestapiApplication.java
+++ src/main/java/com/apergot/springbootrestapi/SpringBootRestapiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class SpringBootRestapiApplication{
+public class SpringBootRestapiApplication { //implements CommandLineRunner
 
     @Autowired
     BCryptPasswordEncoder passwordEncoder;
@@ -15,10 +15,10 @@ public class SpringBootRestapiApplication{
     public static void main(String[] args) {
         SpringApplication.run(SpringBootRestapiApplication.class, args);
     }
-
-    /* Simplest way to create passwords for users using bycrypt, needs to implement CommandLineRunner
+    /*
+    //Simplest way to create passwords for users using bycrypt, needs to implement CommandLineRunner
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         String password = "";
         for (int i = 0; i< 4; i++) {
             String passwordBcrypt = passwordEncoder.encode(password);
